ccc7dd9dde9f322d56df565ea48d5135c382be17
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-05-14 16:30:06 +0000

    print chunks

---
+++ getAndPrintHTML.js
@@ -0,0 +1,29 @@
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+  var acquire = require('https');
+  var chunk = [];
+   acquire.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      chunk.push(data);
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.', chunk.join(''));
+  });
+
+});
+}
+getAndPrintHTML();
